d1d86f95dba99b9e6e327fca30f92204e85d9f16
JB-Dev <jibaoke@gmail.com>
2022-08-17 13:08:50 +0200

    affichage des produit ondex ok

--- front/js/index.js
+++ front/js/index.js
@@ -8,12 +8,31 @@ const fetchKanap = async () => {
     .then((res) => res.json())
     //traite la promise
     .then((promise) => {
-        //stock tableau promise dans kanapData
-        KanapsData = promise
-        console.log(KanapsData);
+      //stock tableau promise dans kanapData
+      KanapsData = promise;
+      console.log(KanapsData);
     });
 };
 
-fetchKanap();
+//function affichage des Kanap
 
-//function affichage
+const kanapDisplay = async () => {
+    //attendre la reponse de fetchKanap
+  await fetchKanap();
+    //recupere la div pour creation element kanap
+    //Creation boucle en fonction du nombre article en BD et recuperation des argument 
+    document.getElementById('items').innerHTML = KanapsData.map(
+      (kanap) =>
+        `<a href="./product.html${kanap._id}">
+                <article>
+                <img src=${kanap.imageUrl} alt=${kanap.altTxt}>
+                <h3 class="productName">${kanap.name}</h3>
+                <p class="productDescription">Prix : ${kanap.price} €</p>
+                <p class="productDescription">${kanap.description}</p>
+                </article>
+            </a>`
+    );
+
+};
+
+kanapDisplay();
